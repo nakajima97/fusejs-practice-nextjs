86d0c11ba8abb1ad35760a7a1da9150a03e84d5e
Nakajima <kituneudon97@gmail.com>
2024-07-04 22:26:39 +0900

    従業員一覧の作成 (#4)
    * #3 従業員一覧を作成
    
    * #3 従業員一覧を表示するようにした

---
+++ src/features/Employee/components/EmployeeList/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { List } from "@mui/material";
+
+import { EmployeeListItem } from "../EmployeeListItem";
+import { Employees } from "../../types";
+
+type Props = {
+  employees: Employees;
+};
+
+export const EmployeeList = ({ employees }: Props) => {
+  return (
+    <List>
+      {employees.map((employee) => (
+        <EmployeeListItem key={employee.id} {...employee} />
+      ))}
+    </List>
+  );
+};
